38df78d23c07e3f8481bc0a2b6525894d51814b2
Evillos <74408849+Evillos@users.noreply.github.com>
2021-02-16 13:37:40 +0600

    Задачи на разветвляющийся алгоритмы

---
+++ 5 задача.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import math
+x=input('x=')
+x=int(x)
+if x>=0:
+    y=math.sqrt(x)
+elif x>=0:
+        y=x**2
+print('x=',x)
+print('y=',y)
+
+
+# In[3]:
+
+
+import math
+x=input('x=')
+x=int(x)
+if x>=0:
+    y=math.sqrt(x)
+elif x>=0:
+        y=x**2
+print('x=',x)
+print('y=',y)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+import math
+x=input('x=')
+x=int(x)
+if x>=0:
+    y=math.sqrt(x)
+elif x>=0:
+        y=x**2
+print('x=',x)
+print('y=',y)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+

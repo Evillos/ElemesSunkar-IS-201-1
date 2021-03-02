2a2392cbceb3c2c21f6a41d85e3e17f9f5e6f72a
Evillos <74408849+Evillos@users.noreply.github.com>
2021-03-02 09:15:19 +0600

    Лаб работа 3 и 4

---
+++ CALCULATER.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#calculator
+what = input("что делаем ?(+-):")
+a=float(input("1 число "))
+b=float(input("2 число "))
+if what == "+":
+    c = a + b
+    print ("Результат:" + str(c))
+elif what == "-":
+    c= a - b
+print ("Результат:" + str(c))
+
+
+# In[ ]:
+
+
+
+

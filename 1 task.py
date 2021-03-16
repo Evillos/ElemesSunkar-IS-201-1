df265f1f10989db01cdbb54411267586803dc731
Evillos <74408849+Evillos@users.noreply.github.com>
2021-03-16 14:06:16 +0600

    Рубежка

---
+++ 1 task.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=5
+b=5.0
+c="5"
+res=float(a)+b+float(c)
+print(res)
+
+
+# In[2]:
+
+
+a=5
+print(type(5))
+
+
+# In[3]:
+
+
+a=5.0
+print(type(a))
+
+
+# In[4]:
+
+
+a=5
+print(type("5"))
+
+
+# In[ ]:
+
+
+
+

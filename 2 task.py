1018540c5b4a3e4eefe0a8d3e536c4af4a9a3b85
Evillos <74408849+Evillos@users.noreply.github.com>
2021-03-16 14:06:48 +0600

    Рубежка

---
+++ 2 task.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[14]:
+
+
+x=(input("потери"))
+x=int(x)
+y=(input("прибыль"))
+y=int(y)
+if x>y:
+    res=x-y
+    print("ваши убытки составляют=",res)
+elif x<y:
+    res=x+y
+    print("ваш прибыль составляет=",res)
+    
+
+
+
+# In[ ]:
+
+
+
+
